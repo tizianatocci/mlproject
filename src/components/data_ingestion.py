3b79a3c97ab160e33f3ca4e8a9b303d6ce852c4e
tizianatocci <tiziana.tocci96@hotmail.it>
2026-07-21 12:33:34 +0200

    hyperparameters testing

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -60,3 +60,4 @@ if __name__ == "__main__":
 
     model_trainer=ModelTrainer()
     print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 from src.exception import CustomException
 import dill 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -21,21 +22,26 @@ def save_object(file_path, obj):
         raise CustomException(e,sys)
     
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try: 
         report = {}
 
-        for i in range(len(list(models))):
-            model = list (models.values())[i]
+        for model_name, model in models.items():
+            para = params.get(model_name, {})
 
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train) #train model
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
 
         return report
     
