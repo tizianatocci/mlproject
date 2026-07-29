daccf3194eea0b212532b0215d2e146f3651e7e0
tizianatocci <tiziana.tocci96@hotmail.it>
2026-07-29 19:08:58 +0200

    updated application.py

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import pickle 
 from flask import Flask, request, render_template
 import numpy as np
-import pandas as pd
+import pandas as pd 
 
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
